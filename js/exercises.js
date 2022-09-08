6f10ab33a2e4d38033ef9fb7ee382885baaf8183
Jonathan Grant <jonathangrant@Jonathans-MacBook-Air.local>
2022-09-07 20:01:03 -0400

    updated last question

--- js/exercises.js
+++ js/exercises.js
@@ -23,11 +23,12 @@ function max(num1, num2 ) {
       return num1 
       }
       if(num2 > num1 && num2 > num3) {
-        return num2}
-        if(num3 > num1 && num3 > num3) {
+        return num2
+      }
+        if(num3 > num1 && num3 > num2) {
           return num3
         }
-    }
+    
   }
 
 
